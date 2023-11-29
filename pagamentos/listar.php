f2f0c582305f815bb0eb29c5cab8df30f308a544
smbresolin <scheila.bresolin@hotmail.com>
2023-11-29 13:29:31 -0300

    Correção de botões

--- pagamentos/listar.php
+++ pagamentos/listar.php
@@ -29,6 +29,7 @@ include '../layout/header.php';
                 <th>Condição de Pagamento</th>
                 <th>Criado em</th>
                 <th>Atualizado em</th>
+                <th>Ações</th>
             </tr>
         </thead>
         <tbody>
@@ -41,7 +42,7 @@ include '../layout/header.php';
                     echo "<td>" . $row["condicao"] . "</td>";
                     echo "<td>" . date('d/m/Y H:i:s', strtotime($row["criado_em"])) . "</td>";
                     echo "<td>" . date('d/m/Y H:i:s', strtotime($row["atualizado_em"])) . "</td>";
-                    echo "<td><a href='editar.php?id=" . $row["id"] . "'>Editar</a> | <a href='deletar.php?id=" . $row["id"] . "'>Deletar</a></td>";
+                    echo "<td><a href='editar.php?id=" . $row["id"] . "'>Editar</a> |<a class='deletar' href='deletar.php?id=" . $row["id"] . "'>Deletar</a></td>";
                     echo "</tr>";
                 }
             } else {

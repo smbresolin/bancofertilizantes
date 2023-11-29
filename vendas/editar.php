883099566da9b7b2b1381a0475f7156159cf883f
smbresolin <scheila.bresolin@hotmail.com>
2023-11-29 13:23:45 -0300

    Correção de código

--- vendas/editar.php
+++ vendas/editar.php
@@ -18,14 +18,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 } else {
     $id = $_GET['id'];
-    $sql_vendas = "SELECT id, data_venda, cliente, vendedor, produto, pagamento FROM vendas WHERE id=$id";
-    $result = $conexao->query($sql_vendas);
-    if ($result->num_rows > 0) {
-        $vendas = $result->fetch_assoc();
-    } else {
-        echo "Venda não encontrada!";
-        exit;
-    }
+$sql_vendas = "SELECT v.id, v.data_venda, v.cliente, v.vendedor, 
+    p.nome AS produto_nome,
+    tp.tipo AS pagamento_nome, v.criado_em, c.nome AS cliente_nome, vd.nome AS vendedor_nome
+    FROM vendas as v 
+    LEFT JOIN clientes as c ON v.cliente = c.id
+    LEFT JOIN vendedores as vd ON v.vendedor = vd.id
+    LEFT JOIN produtos as p ON v.produto = p.id 
+    LEFT JOIN pagamentos as tp ON v.pagamento = tp.id
+    WHERE v.id='$id'"; 
+
+$result = $conexao->query($sql_vendas);
+if ($result->num_rows > 0) {
+    $vendas = $result->fetch_assoc();
+} else {
+    echo "Venda não encontrada!";
+    exit;
+}
 }
 $conexao->close();
 ?>
@@ -48,10 +57,10 @@ include '../layout/header.php';
 
 <form method="post" action="editar.php">
     <input type="hidden" name="id" value="<?php echo $vendas['id']; ?>">
-    Data da Venda: <input type="text" name="data_venda" value="<?php echo $vendas['data_venda']; ?>"><br>
-    Cliente: <input type="text" name="cliente" value="<?php echo $vendas['cliente']; ?>"><br>
-    Vendedor: <textarea name="vendedor"><?php echo $vendas['vendedor']; ?></textarea><br>
-    Produto: <input type="text" name="produto" value="<?php echo $vendas['produto']; ?>"><br>
-    Pagamento: <input type="text" name="pagamento" value="<?php echo $vendas['pagamento']; ?>"><br>
+    Data da Venda: <input type="text" name="data_venda" value="<?php echo isset($vendas['data_venda']) ? $vendas['data_venda'] : ''; ?>"><br>
+    Cliente: <input type="text" name="cliente" value="<?php echo isset($vendas['cliente_nome']) ? $vendas['cliente_nome'] : ''; ?>"><br>
+    Vendedor: <textarea name="vendedor"><?php echo isset($vendas['vendedor_nome']) ? $vendas['vendedor_nome'] : ''; ?></textarea><br>
+    Produto: <input type="text" name="produto" value="<?php echo isset ($vendas['produto_nome']) ? $vendas['produto_nome'] : ''; ?>"><br>
+    Pagamento: <input type="text" name="pagamento" value="<?php echo isset($vendas['pagamento_nome']) ? $vendas['pagamento_nome'] : ''; ?>"><br>
     <input type="submit" value="Salvar">
 </form>

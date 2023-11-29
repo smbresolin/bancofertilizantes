d066e28d4ec494748bfc4dac82b1746577ccea8c
smbresolin <scheila.bresolin@hotmail.com>
2023-11-29 13:31:24 -0300

    Correção de Botões

--- clientes/listar.php
+++ clientes/listar.php
@@ -19,6 +19,7 @@ include '../layout/header.php';
 </head>
 
 <body>
+<link rel="stylesheet" href="../css/clientes.css">
     <h1>Clientes</h1>
     <a href="adicionar.php">Adicionar Novo Cliente</a>
     <table border="1">
@@ -32,6 +33,7 @@ include '../layout/header.php';
                 <th>Telefone</th>
                 <th>Criado em</th>
                 <th>Atualizado em</th>
+                <th>Ações</th>
             </tr>
         </thead>
         <tbody>
@@ -47,7 +49,7 @@ include '../layout/header.php';
                     echo "<td>" . $row["telefone"] . "</td>";
                     echo "<td>" . date('d/m/Y H:i:s', strtotime($row["criado_em"])) . "</td>";
                     echo "<td>" . date('d/m/Y H:i:s', strtotime($row["atualizado_em"])) . "</td>";
-                    echo "<td><a href='editar.php?id=" . $row["id"] . "'>Editar</a> | <a href='deletar.php?id=" . $row["id"] . "'>Deletar</a></td>";
+                    echo "<td><a href='editar.php?id=" . $row["id"] . "'>Editar</a> |<a class='deletar' href='deletar.php?id=" . $row["id"] . "'>Deletar</a></td>";
                     echo "</tr>";
                 }
             } else {
